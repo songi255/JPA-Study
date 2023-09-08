9a85e6662ffc8e4f7dab94e94493d197b7ff66a2
master <assert2914@gmail.com>
2023-09-08 11:04:02 +0900

    section 4

---
+++ src/main/java/org/example/model/entity/Order.java
@@ -0,0 +1,26 @@
+package org.example.model.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "ORDERS")
+@Getter
+@Setter
+public class Order {
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+}
---
+++ src/main/java/org/example/model/entity/OrderStatus.java
@@ -0,0 +1,5 @@
+package org.example.model.entity;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
